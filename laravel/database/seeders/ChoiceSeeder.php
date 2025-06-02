c36e0fdb0978ccb27165e56fd8d65613912c3ff7
Cedrine MacRempl <cedrine.tille@gmail.com>
2025-06-02 10:00:12 +0200

    Create seeders files

---
+++ laravel/database/seeders/ChoiceSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ChoiceSeeder extends Seeder
+{
+    public function run()
+    {
+        $choices = json_decode(file_get_contents(database_path('seeders/data/choices.json')), true);
+
+        foreach ($choices as $choice) {
+            DB::table('choices')->insert([
+                'id' => $choice['id'],
+                'question_id' => $choice['question_id'],
+                'text_answer' => $choice['text_answer'],
+                'img_answer' => $choice['img_answer']
+            ]);
+        }
+    }
+}
---
+++ laravel/database/seeders/TheorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TheorySeeder extends Seeder
+{
+    public function run()
+    {
+        $theories = json_decode(file_get_contents(database_path('seeders/data/theories.json')), true);
+
+        foreach ($theories as $theory) {
+            DB::table('theories')->insert([
+                'id' => $theory['id'],
+                'lesson_id' => $theory['lesson_id'],
+                'content' => $theory['content'],
+                'position_order' => $theory['position_order']
+            ]);
+        }
+    }
+}
