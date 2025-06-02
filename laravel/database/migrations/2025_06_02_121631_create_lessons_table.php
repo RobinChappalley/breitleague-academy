2f20569d007a895ccfdd372ab72914fe70d748ec
Cedrine MacRempl <cedrine.tille@gmail.com>
2025-06-02 14:17:16 +0200

    Create files for lessons

---
+++ laravel/database/migrations/2025_06_02_121631_create_lessons_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('module_id');
+            $table->integer('order_position')->default(1);
+            $table->timestamps();
+
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('lessons');
+    }
+};
