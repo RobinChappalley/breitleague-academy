7e4f3f6b5ff619806136f7b7a30d9cc47bef7528
Robin Chappalley <robin@chapi.ch>
2025-06-03 11:24:58 +0200

    Add and centered watch image

--- frontend/src/views/learning/Formation.vue
+++ frontend/src/views/learning/Formation.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="image-container">
-    <img alt="image aviateurs" src="/backgrounds/aviators-horizontal.png" class="desktop-image">
-    <img alt="image aviateurs" src="/backgrounds/aviators-vertical.png" class="mobile-image">
+
+  <div class="formation-image-container">
+    <img alt="watch aviators" src="/backgrounds/aviators-watch.png" class="lesson-watch">
   </div>
 </template>
 
 <script>
 </script>
 
-<style scoped>
-.image-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
+<style>
+:root {
+  --aviators-vertical: url('/backgrounds/aviators-vertical.png');
+  --aviators-horizontal: url('/backgrounds/aviators-horizontal.png');
+  --aviators-watch: url('/backgrounds/aviators-watch.png');
 }
+
+.formation-image-container {
+  height: 100vh;
+  width: 100vw;
+  background-position: right bottom;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  @media screen and (max-width: 768px){
+    background-image: var(--aviators-vertical);
+
+  }
+  @media screen and (min-width: 768px){
+    background-image: var(--aviators-horizontal);
+
+  }
+}
+.lesson-watch {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  max-width: 65vw;
+  max-height: 85vh;
+}
+
 
 .desktop-image,
 .mobile-image {
