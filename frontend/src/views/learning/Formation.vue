a59fe89baaaba3b285404d43954199ed4c32ffc3
Robin Chappalley <robin@chapi.ch>
2025-06-03 14:45:30 +0200

    Position checkpoint button

--- frontend/src/views/learning/Formation.vue
+++ frontend/src/views/learning/Formation.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="formation-image-container">
+    <div class="formation-watch-container">
     <img alt="watch aviators" src="/backgrounds/aviators-watch.png" class="lesson-watch">
+    <button class="checkpoint-button"></button>
+    </div>
   </div>
 </template>
 
@@ -14,11 +17,15 @@
 }
 
 .formation-image-container {
+  position :relative;
   height: 100vh;
   width: 100vw;
   background-position: right bottom;
   background-repeat: no-repeat;
   background-size: cover;
+  display: flex;
+  align-items: center;
+
 
   @media screen and (max-width: 768px){
     background-image: var(--aviators-vertical);
@@ -29,12 +36,24 @@
 
   }
 }
+
+.formation-watch-container {
+  position: relative;
+  padding: 20px 0;
+}
 .lesson-watch {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
   max-width: 65vw;
   max-height: 85vh;
+}
+.checkpoint-button {
+  height: 4em;
+  width: 4em;
+  background-color: goldenrod;
+  border-radius: 50%;
+  filter:drop-shadow( 0 0 10px black);
+  border: none;
+  position: absolute;
+  left : 30%;
 }
 
 </style>
