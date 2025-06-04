af10ec5ff124a0acaa71fa0689bfe82ee403de14
Robin Chappalley <robin@chapi.ch>
2025-06-04 14:59:37 +0200

    Remove double import of collectionView

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,12 +9,11 @@ import LessonsListView from "@/views/learning/LessonsListView.vue";
 
 export const routes = [
     {path: '/', component: FormationView, meta: {hideNavBar: false}},
-    {path: '/battle', component: BattleView, meta:{hideNavBar: false}},
-    {path: '/collection', component: HomeView, meta: {hideNavBar: false}},
-    {path: '/collection', component: CollectionView, meta :{hideNavBar:false}},
-    {path:'/ranking', component: RankingView, meta: {hideNavBar: false}},
-    {path:'/profile', component: ProfileView, meta: {hideNavBar: false}},
-    {path:'/ressources', component: LessonsListView, meta: {hideNavBar: false}},
-    {path:'/missions', component: MissionsView, meta: {hideNavBar: true}}
+    {path: '/battle', component: BattleView, meta: {hideNavBar: false}},
+    {path: '/collection', component: CollectionView, meta: {hideNavBar: false}},
+    {path: '/ranking', component: RankingView, meta: {hideNavBar: false}},
+    {path: '/profile', component: ProfileView, meta: {hideNavBar: false}},
+    {path: '/ressources', component: LessonsListView, meta: {hideNavBar: false}},
+    {path: '/missions', component: MissionsView, meta: {hideNavBar: true}}
 
 ]
