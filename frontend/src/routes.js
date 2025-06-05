83152ef7b738ea7e157c6f086609be7c0b0cd969
Robin Chappalley <robin@chapi.ch>
2025-06-05 09:29:19 +0200

    Fix merge text fouting le bordel in routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,13 +8,11 @@ import ProfileView from "@/views/profile/ProfileView.vue";
 import CollectionView from "@/views/collection/CollectionView.vue";
 import MissionsView from "@/views/missions/MissionsView.vue";
 import LessonsListView from "@/views/learning/LessonsListView.vue";
-<<<<<<< HEAD
 import CheckpointView from "@/views/learning/CheckpointView.vue";
 import CheckpointQuizView from "@/views/learning/CheckpointQuizView.vue";
 import CheckpointResultsView from "@/views/learning/CheckpointResultsView.vue";
-=======
 import HistoireView from "@/views/learning/HistoireView.vue";
->>>>>>> bbceb37 (Add new ressources)
+
 
 export const routes = [
     {path: '/', component: FormationView, meta: {hideNavBar: false}},
@@ -26,12 +24,9 @@ export const routes = [
     {path: '/profile', component: ProfileView, meta: {hideNavBar: false}},
     {path: '/ressources', component: LessonsListView, meta: {hideNavBar: true}},
     {path: '/missions', component: MissionsView, meta: {hideNavBar: true}},
-<<<<<<< HEAD
     {path: '/checkpoint', component: CheckpointView, meta: {hideNavBar: false}},
     {path: '/checkpoint-quiz', component: CheckpointQuizView, meta: {hideNavBar: true}},
-    {path: '/checkpoint-results', name: 'CheckpointResults', component: CheckpointResultsView, meta: {hideNavBar: true}}
-=======
+    {path: '/checkpoint-results', name: 'CheckpointResults', component: CheckpointResultsView, meta: {hideNavBar: true}},
     {path: '/ressources/history', component: HistoireView, meta: {hideNavBar: true}},
-
->>>>>>> bbceb37 (Add new ressources)
+    
 ]
