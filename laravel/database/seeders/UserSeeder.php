9282f13d65d452c41b5987effa9c469f14320842
Cedrine MacRempl <cedrine.tille@gmail.com>
2025-06-10 11:47:06 +0200

    Add seeder for progression in user

--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Progression;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -13,22 +14,28 @@ class UserSeeder extends Seeder
         $path = database_path('seeders/data/users.json');
         $users = json_decode(file_get_contents($path), true);
 
-        foreach ($users as $user) {
-            DB::table('users')->insert([
-                'id' => $user['id'],
-                'username' => $user['username'],
-                'email' => $user['email'],
+        foreach ($users as $userData) {
+            // Crée l'utilisateur
+            $user = User::create([
+                'id' => $userData['id'], // si tu veux forcer l'id (sinon Laravel l'auto-incrémente)
+                'username' => $userData['username'],
+                'email' => $userData['email'],
                 'password' => Hash::make('123'),
-                'pos_id' => $user['pos_id'],
-                'signup_year' => $user['signup_year'],
-                'avatar' => $user['avatar'],
-                'elo_score' => $user['elo_score'],
-                'battle_won' => $user['battle_won'],
-                'battle_lost' => $user['battle_lost'],
-                'number_available_slots' => $user['number_available_slots'],
-                'is_BS' => $user['is_BS'],
-                'created_at' => now(),
-                'updated_at' => now(),
+                'pos_id' => $userData['pos_id'],
+                'signup_year' => $userData['signup_year'],
+                'avatar' => $userData['avatar'],
+                'elo_score' => $userData['elo_score'],
+                'battle_won' => $userData['battle_won'],
+                'battle_lost' => $userData['battle_lost'],
+                'number_available_slots' => $userData['number_available_slots'],
+                'is_BS' => $userData['is_BS'],
+            ]);
+
+            // Crée la progression liée
+            $user->progression()->create([
+                'last_lesson_id' => 0,
+                'last_checkpoint_id' => 0,
+                'idofquestionscorrectlyanswered' => [],
             ]);
         }
     }
